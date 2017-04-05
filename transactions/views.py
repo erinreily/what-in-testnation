1506909a0a168091856c108e091a09dbc075cc7b
Erin Dunnahoo <erdunnahoo@gmail.com>
2017-04-05 16:54:27 -0500

    Update views.py

--- transactions/views.py
+++ transactions/views.py
@@ -10,5 +10,5 @@ from django.http import HttpResponse
 
 
 def transactions_form(request):
-    #return HttpResponse("You have reached the Transactions page.")
-    return render(request, "transactions/transactions.html")
+    return HttpResponse("You have reached the Transactions page.")
+    #return render(request, "transactions/transactions.html")
